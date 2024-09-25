3d6ee9ac1017b8e120fe65a97783fb58a556b177
antonioir1218 <antonioixmatlahua0@gmail.com>
2024-09-25 10:05:01 -0600

    add interests branch #6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,13 @@ import { WorkExperienceComponent } from './work-experience/work-experience.compo
 import { EducationComponent } from './education/education.component';
 import { SkillsComponent } from './skills/skills.component';
 import { LanguajesComponent } from './languajes/languajes.component';
+import { InterestsComponent } from './interests/interests.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent,EducationComponent,SkillsComponent,LanguajesComponent],
+  imports: [RouterOutlet, HeaderComponent, WorkExperienceComponent,EducationComponent,SkillsComponent,LanguajesComponent,InterestsComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
